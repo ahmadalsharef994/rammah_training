c2024fa5f500e656edb49c3dafdea897a85bd05d
ahmadalsharef994 <ahmadalsharef2021@gmail.com>
2024-02-20 12:55:35 +0200

    test lock branch & require pull

--- university.route.js
+++ university.route.js
@@ -5,7 +5,7 @@ const universityController = require('./university.controller');
 // Create a new University
 router.post('/universities', universityController.createUniversity);
 
-// Get all Universities
+// Get all Universities.
 router.get('/universities', universityController.getAllUniversities);
 
 // Get a single University by id
